f52a8b7a63c88ce8fbac310ee5ecfe2d547096ac
fschrader1992 <fschrader1992@gmail.com>
2017-07-21 12:12:36 +0200

    added frame

--- image_creation_dot.py
+++ image_creation_dot.py
@@ -6,8 +6,9 @@ from microsaccades_functions import *
 #---------------------------------------------------------------------------------------IMAGE-CREATION
 
 image_size = 240
-direction = 13.*np.pi/8. # dot moving direction in arc (get right velocities !)
+direction = 13.*np.pi/8. #dot moving direction in arc (get right velocities !)
 radius = 4. #dot size of 4arcmin -> 8px
+rect_size = 110. #size of inner radius 
 
 vel = 0.06 #velocity is 30 arcmin/s -> 60px/1000ms
 
@@ -26,18 +27,24 @@ tl_x, tl_y = sigma*np.random.multivariate_normal(mean, cov, film_length).T
 
 
 pos = (image_size/2.+0.5,image_size/2+0.5)
-#maybe here a loop to make it last even longer
+center = pos #for rectangle
+
+#loop through the 
 for f in range(film_length):
     canvas = np.zeros((image_size, image_size))
     
     #update position
     #for dot moving task-----------------------
-    pos = (pos[0]+vel*np.sin(direction),pos[1]+vel*np.cos(direction))
+    #pos = (pos[0]+vel*np.sin(direction),pos[1]+vel*np.cos(direction))
     #------------------------------------------
+	
     #for random eye movements------------------
     #pos = (pos[0]+tl_y[f],pos[1]+tl_x[f])
     #------------------------------------------
-    
+	
+	#for moving rectangle frame----------------
+    #center = (center+tl_y[f],center+tl_x[f])
+    #------------------------------------------
     
     for i in range(image_size):
         for j in range(image_size):
@@ -46,6 +53,10 @@ for f in range(film_length):
                 canvas[i,j] = 1
             elif 3.5 < dist and dist  < 4.5:
                 canvas[i,j] = 4.5 - dist
+			#rectangle-(exp2)--------------------------
+			#if abs(float(i)-center) > rect_size or abs(float(j)-center)> rect_size:
+			#	canvas[i,j] = 1
+			#------------------------------------------
         
     fig = plt.figure()
     fig.set_size_inches(2,2)
