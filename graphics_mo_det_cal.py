376aa49d91d16d7aea6a0ad08fe1a6e362a79f22
fschrader1992 <fschrader1992@gmail.com>
2017-08-28 14:15:29 +0200

    Add files via upload

--- graphics_mo_det_cal.py
+++ graphics_mo_det_cal.py
@@ -18,49 +18,129 @@ import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
-data = np.loadtxt('m_max.data')
+data = np.loadtxt('data/mo_det_cal/m_max_spikes.txt')
 #delay
-delay = data[:,0]
-ds = set(delay)
-#set of maximums
-som = []
-dels = []
+weights = data[:,0]
+ws = set(weights)
 
-for s in ds:
-	#velocity
-	vel = data[:,1]
-	#spike number
-	sp = data[:,2]
+for w in ws:
+    print w
+    data = np.loadtxt('data/mo_det_cal/m_max_spikes.txt')
+    delay = data[:,1]
+    ds = set(delay)
+    #set of maximums
+    som = []
+    dels = []
+    
+    for s in ds:
+        #print 'w '+str(w)
+        #print 's '+str(s)
+        vel=[]
+        sp=[]
+        for l in data:
+            if l[0]==w and l[1]==s:
+                vel+=[l[2]]
+                sp+=[l[3]]
+            #velocity
+            #vel = data[weis==w,2]
+            #spike number
+            #sp = data[weis==w,3]
+        #print 'v: '+str(vel)
+        #vel=vel[delay==s]
+        #sp=sp[delay==s]
+        mf= max(sp)
+        v=vel[sp.index(mf)]
+        #print v
+        dels+=[s]
+        som+=[v]
+        
+        fig = plt.figure(1)
+        ax = fig.add_subplot(111)
+        ax.set_title('midgets, delay '+str(s)+', weight '+str(w))
+        ax.set_xlabel('velocity in arcmin/s')
+        ax.set_ylabel('maximum firingrate in spikes/s')
+        ax.plot(vel,sp,'rx')
+        #print vel
+        #print sp
+        plt.savefig('img/mo_det_cal/midget_resp_d' + str(int(s)) + '_w'+str(int(w))+'.pdf')
+        #plt.show()
+        plt.close()
+        
+    #parasols 
 
-	vel=vel[delay == s]
-	sp=sp[delay == s]
-	mf= max(sp)
-	v=vel[sp==mf]
-	print v
-	dels+=[s]
-	som+=[v[0]]
+    data = np.loadtxt('data/mo_det_cal/p_max_spikes.txt')
+    #delay
+    delay = data[:,1]
+    ds = set(delay)
+    #set of maximums
+    som_p = []
+    dels_p = []
+    
+    for s in ds:
+        #print 'w '+str(w)
+        #print 's '+str(s)
+        vel=[]
+        sp=[]
+        for l in data:
+            if l[0]==w and l[1]==s:
+                vel+=[l[2]]
+                sp+=[l[3]]
+            #velocity
+            #vel = data[weis==w,2]
+            #spike number
+            #sp = data[weis==w,3]
+        #print 'v: '+str(vel)
+        #vel=vel[delay==s]
+        #sp=sp[delay==s]
+        mf= max(sp)
+        v=vel[sp.index(mf)]
+        #print v
+        dels_p+=[s]
+        som_p+=[v]
+        '''
+        #velocity
+        vel = data[weis==w,2]
+        #spike number
+        sp = data[weis==w,3]
 
-	'''fig = plt.figure(1)
+        vel=vel[delay == s]
+        sp=sp[delay == s]
+        mf= max(sp)
+        v=vel[sp==mf]
+        #print v
+        dels_p+=[s]
+        som_p+=[v[0]]
+        '''
+        fig = plt.figure(1)
+        ax = fig.add_subplot(111)
+        ax.set_title('parasols, delay '+str(s)+', weight '+str(w))
+        ax.set_xlabel('velocity in arcmin/s')
+        ax.set_ylabel('maximum firingrate in spikes/s')
+        ax.plot(vel,sp,'bx')
+        plt.savefig('img/mo_det_cal/parasol_resp_d' + str(int(s)) + '_w'+str(int(w))+'.pdf')
+        plt.close()
+        
+    fig = plt.figure(1)
+    #print som
+    #print dels	
+    fig.suptitle('maximum responses for delays with weight '+str(w))
+    ax = fig.add_subplot(121)
+    ax.set_title('midgets')
+    ax.set_xlabel('delay in ms')
+    ax.set_ylabel('maximum response velocity in arcmin/s')
+    ax.plot(dels,som,'rx')
 
-	ax = fig.add_subplot(221)
-	ax.set_title('delay '+str(s))
-	ax.set_xlabel('velocity in arcmin/s')
-	ax.set_ylabel('maximum firingrate in spikes/s')
-	ax.plot(vel,sp, c='b')
 
-	#plt.savefig('.pdf')
+    #fig = plt.figure(1)
+    #print som
+    #print dels	
+    ax = fig.add_subplot(122)
+    ax.set_title('parasols')
+    ax.set_xlabel('delay in ms')
+    #ax.set_ylabel('maximum response velocity in arcmin/s')
+    ax.plot(dels_p,som_p,'bx')
 
-	plt.show()
-	'''
-fig = plt.figure(1)
-print som
-print dels	
-ax = fig.add_subplot(221)
-ax.set_title('maximum responses for delays')
-ax.set_xlabel('delay')
-ax.set_ylabel('maximum response velocity in arcmin/s')
-ax.plot(dels,som,c='g')
 
-#plt.savefig('.pdf')
-
-plt.show()
+    plt.savefig('img/mo_det_cal/responses_w'+str(int(w))+'.pdf')
+    plt.close()
+    #plt.show()
